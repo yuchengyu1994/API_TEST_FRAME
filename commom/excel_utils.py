900978b82f7450fa4cb0866afb0ac3fc9a597c5b
yuchengyu <15575953724@163.com>
2022-07-14 16:47:33 +0800

    第二次提交，新增了读取excel的数据， 并将数据转换成真正需要的接口业务数据的方法

---
+++ commom/excel_utils.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @author:Yuchengyu
+# @time: 2022/7/14 0014 14:05
+import os
+import xlrd2
+
+dir_path = os.path.join(os.path.dirname(__file__), '..','samples/data/test_case.xlsx')
+class ExcelUtils:
+    def __init__(self,sheet_name,excel_path=dir_path):
+        self.excel_path=excel_path
+        self.sheet_name=sheet_name
+        self.sheet=self.get_sheet()
+
+
+    def get_sheet(self):
+        wb = xlrd2.open_workbook(self.excel_path)
+        sheet = wb.sheet_by_name(self.sheet_name)
+        return sheet
+
+    def get_row_count(self):
+        return self.sheet.nrows
+
+    def get_col_count(self):
+        return self.sheet.ncols
+
+    def get_cell_value(self,row_index, col_index):
+        return self.sheet.cell_value(row_index, col_index)
+
+    def get_merge_value(self,row_index, col_index):
+        value = self.get_cell_value(row_index, col_index)
+        for (rlow, rhign, clow, chigh) in self.sheet.merged_cells:
+            if row_index >= rlow and row_index < rhign:
+                if col_index >= clow and col_index < chigh:
+                    value = self.get_cell_value(rlow, clow)
+        return value
+
+    def get_sheet_data_dict(self):
+        all_data_list = []
+        for i in range(1, self.get_row_count()):
+            data_list = {}
+            for j in range(self.get_col_count()):
+                data_list[self.get_merge_value(0, j)] = self.get_merge_value(i, j)
+            all_data_list.append(data_list)
+        return  all_data_list
+
+if __name__=='__main__':
+    excle_utils = ExcelUtils('Sheet1')
+    print(excle_utils.get_sheet_data_dict())
